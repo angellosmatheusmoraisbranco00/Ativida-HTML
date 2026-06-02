fb3815d0ad0e6641800e934e261fb4b0b8169c00
Angellos Matheus <angellosmatheus250@gmail.com>
2026-06-01 22:02:23 -0300

    Mensagem

---
+++ PRATICA-1.html/pratica1.js
@@ -0,0 +1,29 @@
+const botao = document.getElementById("btnMensagem");
+
+    if(!botao){
+
+} else {  
+
+botao.addEventListener("click", function (){
+
+    alert("Olá! O pão de quijo quentinho acaba de sair do forno");
+    
+    console.log("Botão de boas-vindas clicado.");
+});
+}
+
+
+
+const botao1 = document.getElementById("croisante");
+
+    if(!botao1){
+
+} else {
+
+botao1.addEventListener("click", function (){
+
+    alert("Crosaint de chocolate com 20% de desconto hoje!");
+    
+    console.log("Botão de boas-vindas clicado.");
+})
+}
