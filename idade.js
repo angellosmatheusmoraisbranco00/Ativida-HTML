6ee836986853db76e4f72b815774e38c509594ef
angellosmatheusmoraisbranco00 <angellosmatheus250@gmail.com>
2026-06-02 21:42:02 -0300

    atividade.js

---
+++ idade.js
@@ -0,0 +1,55 @@
+/*
+let idade = 25;
+const pais = "brasil";
+
+// Mudando o valor
+
+idade = 26;
+
+console.log(idade); // Exibe 26
+console.log(pais); // Exibe Brasil
+
+//Exeplo de uso no JavaScript
+let preco = 50;
+let quantidade = 3;
+let frete = 15;
+
+// Cálculo matemático
+let total = (preco * quantidade) + frete;
+
+console.log(total);    // Resultado: 165*/
+
+
+let paes = 6;
+const padaria = "doces"
+
+paes = 7;
+
+console.log(paes);  // Exibe 7
+console.log(padaria);   // Exibe doces
+
+//Exemplo de uso no JavaScript
+let preco = 100;
+let quantidade = 3;
+let frete = 15;
+// Cáculo matemático 
+let total = (preco * quantidade) + frete;
+
+console.log(total); //Resultado: 315
+
+
+let A = 5;
+let B = 10;
+let c = A
+A = B
+B = c
+console.log(A ,"a letra A vai receber 10");
+console.log(B ,"a letra B vai receber 5");
+//media
+let d, f, g;
+d = 7;
+f = 8;
+g = 9;
+let media = (d + f + g)
+console.log("A media de", d, "e", f,  "e", g, "e", 8 + media);
+
