51057af50ee8a26a04cf5758fa797b0564969df7
angellosmatheusmoraisbranco00 <angellosmatheus250@gmail.com>
2026-06-09 21:40:52 -0300

    Array

---
+++ ARRAW.js
@@ -0,0 +1,39 @@
+//Array de Strings
+//let frutas = ["Maçãs", "Leite", "pão", "Ovos", "Café"];
+//Array de Números
+//let Notas = [10, 8.5, 7];
+//Array Misto
+//let dados = ["João", 25, true];
+
+
+//console.log(frutas);
+//let total = frutas.length;
+//console.log(total);
+//Mostra a quantidade que tem
+//console.log(frutas[frutas.length - 1]);
+//console.log(frutas[1]);
+//console.log(frutas[3]);
+
+//frutas.push("pera");
+//console.log(frutas[frutas.length - 1]);
+//console.log(total);
+//let ultima = frutas.pop();
+
+//console.log(frutas);
+
+//let frutas = ["maçã", "Banana", "Uva"];
+   // for (let i = 0; i< frutas .length; i++) { 
+    //console.log(frutas[i]);
+    //}
+let produtos = [
+["pão",$5],
+["leite",$4,500],
+["Café",$15]
+];
+//acessando dados:
+let nome = produtos [0] [0]; //"pão"
+let preco = produtos [0] [1]; // 5.00
+// Percorrendo:
+console.log(produtos [1] [0]); //"leite"
+console.log(produtos[2] [0]); //"café"
+console.log(produtos[0] [0]); //"pão"
