ba91f6202ac64d77fc2240ecd2393f18ccb7ac23
angellosmatheusmoraisbranco00 <angellosmatheus250@gmail.com>
2026-06-10 21:46:28 -0300

    UC2

---
+++ revisãoUC2.js
@@ -0,0 +1,17 @@
+// ALGORITMO: calcular a média de 3 notas
+// Passo 1 — receber as notas
+const nota1 = 7.5;
+const nota2 = 8.0;
+const nota3 = 6.5;
+
+// Passo 2 — somar as notas
+const soma = nota1 + nota2 + nota3;
+
+// Passo 3 — dividir pela quantidade de notas
+const quantidade = 3;
+const media = soma / quantidade;
+
+// Passo 4 — exibir o resultado
+console.log("media;", media); // Saída: Média: 7.333...
+;
+
